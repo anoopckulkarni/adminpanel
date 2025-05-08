93b6e4347fe1c18202c913d1f0d9d6493f4f6bec
anoopckulkarni <142482374+anoopckulkarni@users.noreply.github.com>
2025-05-08 19:59:03 +0530

    3rd Fail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import AppRouter from './router';
-import { AuthProvider } from './contexts/AuthContext'; // Corrected path
 import './styles/global.css';
 
 function App() {
-    return (
-        <AuthProvider>
-            <AppRouter />
-        </AuthProvider>
-    );
+    return <AppRouter />;
 }
 
 export default App;
